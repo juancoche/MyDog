d9c6a4791d15e3e9d6674aa869a8bfae4aac2ac0
juancoche <gorrondespoti@gmail.com>
2020-04-28 23:27:36 +0200

    28/04

---
+++ app/src/main/java/com/juancoche/mydogv3/MainActivity.java
@@ -0,0 +1,29 @@
+package com.juancoche.mydogv3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton floatingActionButton =
+                (FloatingActionButton) findViewById(R.id.floating_action_button);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Handle the click.
+            }
+        });
+    }
+
+
+}
